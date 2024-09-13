5889ff6464d063b8fc3ed9466dd25d337f6c1f03
marcojalenyu <112309140+marcojalenyu@users.noreply.github.com>
2024-09-13 21:17:29 +0800

    Updated the command template message and header

--- main.cpp
+++ main.cpp
@@ -14,60 +14,29 @@ using namespace std;
 
 /// @brief Prints the banner of the program
 void printHeader() {
-	//  .d8888b.   .d8888b.   .d88888b.  8888888b.  8888888888 .d8888b. Y88b   d88P
-	// d88P  Y88b d88P  Y88b d88P" "Y88b 888   Y88b 888       d88P  Y88b Y88b d88P 
-	// 888    888 Y88b.      888     888 888    888 888       Y88b.       Y88o88P  
-	// 888         "Y888b.   888     888 888   d88P 8888888    "Y888b.     Y888P   
-	// 888            "Y88b. 888     888 8888888P"  888           "Y88b.    888    
-	// 888    888       "888 888     888 888        888             "888    888    
-	// Y88b  d88P Y88b  d88P Y88b. .d88P 888        888       Y88b  d88P    888    
-	//  "Y8888P"   "Y8888P"   "Y88888P"  888        8888888888 "Y8888P"     888    
-
-	// cout << " .d8888b.   .d8888b.   .d88888b.  8888888b.  8888888888 .d8888b. Y88b   d88P\n";
-	// cout << "d88P  Y88b d88P  Y88b d88P\" \"Y88b 888   Y88b 888       d88P  Y88b Y88b d88P \n";
-	// cout << "888    888 Y88b.      888     888 888    888 888       Y88b.       Y88o88P  \n";
-	// cout << "888         \"Y888b.   888     888 888   d88P 8888888    \"Y888b.     Y888P   \n";
-	// cout << "888            \"Y88b. 888     888 8888888P\"  888           \"Y88b.    888    \n";
-	// cout << "888    888       \"888 888     888 888        888             \"888    888    \n";
-	// cout << "Y88b  d88P Y88b  d88P Y88b. .d88P 888        888       Y88b  d88P    888    \n";
-	// cout << " \"Y8888P\"   \"Y8888P\"   \"Y88888P\"  888        8888888888 \"Y8888P\"     888    \n";
-
-	//  .d8888b.   .d8888b.            .d88888b.   .d88888b.  8888888b.   .d8888b. Y88b   d88P
-	// d88P  Y88b d88P  Y88b          d88P" "Y88b d88P" "Y88b 888   Y88b d88P  Y88b Y88b d88P 
-	// 888    888 Y88b.               888     888 888     888 888    888 Y88b.       Y88o88P  
-	// 888         "Y888b.            888     888 888     888 888   d88P  "Y888b.     Y888P   
-	// 888            "Y88b.          888     888 888     888 8888888P"      "Y88b.    888    
-	// 888    888       "888          888     888 888     888 888              "888    888    
-	// Y88b  d88P Y88b  d88P          Y88b. .d88P Y88b. .d88P 888        Y88b  d88P    888    
-	//  "Y8888P"   "Y8888P"            "Y88888P"   "Y88888P"  888         "Y8888P"     888    
-
-	cout << " .d8888b.   .d8888b.            .d88888b.   .d88888b.  8888888b.   .d8888b. Y88b   d88P\n";
-	cout << "d88P  Y88b d88P  Y88b          d88P\" \"Y88b d88P\" \"Y88b 888   Y88b d88P  Y88b Y88b d88P \n";
-	cout << "888    888 Y88b.               888     888 888     888 888    888 Y88b.       Y88o88P  \n";
-	cout << "888         \"Y888b.            888     888 888     888 888   d88P  \"Y888b.     Y888P   \n";
-	cout << "888            \"Y88b.          888     888 888     888 8888888P\"      \"Y88b.    888    \n";
-	cout << "888    888       \"888          888     888 888     888 888              \"888    888    \n";
-	cout << "Y88b  d88P Y88b  d88P          Y88b. .d88P Y88b. .d88P 888        Y88b  d88P    888    \n";
-	cout << "\"Y8888P\"    \"Y8888P\"            \"Y88888P\"   \"Y88888P\"  888         \"Y8888P\"     888    \n";
-
+	cout << "  ____ ____   ___  ____  _____ ______   __\n";
+	cout << " / ___/ ___| / _ \\|  _ \\| ____/ ___\\ \\ / /\n";
+	cout << "| |   \\___ \\| | | | |_) |  _| \\___ \\\\ V / \n";
+	cout << "| |___ ___) | |_| |  __/| |___ ___) || |  \n";
+	cout << "\\____|_____/ \\___/|_|   |_____|____/ |_|  \n";
 	cout << "\033[1;32m" << "Hello, Welcome to CSOPESY commandline!\n";
 	cout << "\033[1;33m" << "Type 'exit' to quit, 'clear' to clear the screen\n";
 }
 
 void initialize() {
-	cout << "initialize command recognized. Do something.\n";
+	cout << "initialize command recognized. Doing something.\n";
 }
 void screen() {
-	cout << "screen command recognized. Do something.\n";
+	cout << "screen command recognized. Doing something.\n";
 }
 void schedulerTest() {
-	cout << "scheduler-test command recognized. Do something.\n";
+	cout << "scheduler-test command recognized. Doing something.\n";
 }
 void schedulerStop() {
-	cout << "scheduler-stop command recognized. Do something.\n";
+	cout << "scheduler-stop command recognized. Doing something.\n";
 }
 void reportUtil() {
-	cout << "report-util command recognized. Do something.\n";
+	cout << "report-util command recognized. Doing something.\n";
 }
 
 // /// @brief Prints the list of commands
@@ -84,13 +53,14 @@ void reportUtil() {
 
 /// @brief Clears the screen and reprints header text
 void clear() {
+	cout << "clear command recognized. Doing something.\n";
 	system("cls");
 	printHeader();
 }
 
 /// @brief Exits the program
 void exit() {
-	cout << "Exiting program...\n";
+	cout << "exit command recognized. Doing something.\n";
 	exit(0);
 }
 
